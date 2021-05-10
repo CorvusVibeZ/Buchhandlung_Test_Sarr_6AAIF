4ec6826150b5026411b8d5901e6288711c68a1e5
CorvusVibeZ <72435706+CorvusVibeZ@users.noreply.github.com>
2021-05-11 00:22:34 +0200

    Packagestruktur wurde erstellt

---
+++ buchbestellungsms/src/main/java/at/itkollegimst/sarr/pos1makro/test2/buchbestellungsms/domain/model/aggregates/Bestellung.java
@@ -0,0 +1,4 @@
+package at.itkollegimst.sarr.pos1makro.test2.buchbestellungsms.domain.model.aggregates;
+
+public class Bestellung {
+}
---
+++ buchbestellungsms/src/main/java/at/itkollegimst/sarr/pos1makro/test2/buchbestellungsms/domain/model/aggregates/BestellungsId.java
@@ -0,0 +1,7 @@
+package at.itkollegimst.sarr.pos1makro.test2.buchbestellungsms.domain.model.aggregates;
+
+
+
+
+public class BestellungsId {
+}
